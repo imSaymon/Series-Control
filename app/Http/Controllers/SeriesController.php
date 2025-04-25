9cad868f69037f887183bfacb0064e003714c839
Saymon Rodrigues <saysykes20@gmail.com>
2025-04-25 00:37:16 -0300

    proj controle-series completed

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,22 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Serie;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
-        $series = [
-                'Punisher',
-                'Lost',
-                'Greys Anatomy'
-            ];
-            return \view('series.index')->with('series', $series);
+        $series = Serie::query()->orderBy('nome')->get();
+
+        return \view('series.index')->with('series', $series);
+            
     }
 
     public function create()
     {
         return \view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+        $nomeSerie = $request->input('nome');
+        $serie = new Serie();
+        $serie->nome = $nomeSerie;
+        $serie->save();
+
+        return \redirect('/series');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ Route::get('/', function () {
 
 Route::get('/series', [SeriesController::class, 'index']);
 Route::get('/series/criar', [SeriesController::class, 'create']);
+Route::post('/series/salvar', [SeriesController::class, 'store']);
 
 Route::get('/ola', function () {
     echo "Today is " . date("Y/m/d") . "<br>";
